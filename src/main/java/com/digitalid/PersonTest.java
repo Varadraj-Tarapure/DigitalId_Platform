c3505cd656ddce3bd13591ecde5be6d1ba9ababd
Naghullan02 <s4138471@student.rmit.edu.au>
2026-02-18 15:15:31 +0800

    Adding first jUnit test case

--- src/main/java/com/digitalid/PersonTest.java
+++ src/main/java/com/digitalid/PersonTest.java
@@ -3,6 +3,7 @@ package com.digitalid;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.beans.Transient;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -17,4 +18,12 @@ public class PersonTest {
     void cleanFile() throws IOException {
         Files.deleteIfExists(FILE_PATH);
     }
+
+    @Test
+    void addID_ValidPerson_returnsTrue() {
+        Person p = new Person("56s_d%&fAB", "Ali", "Tan", "32| Highland Street|Melbourne|Victoria|Australia", "15-11-1990");
+        assertTrue(p.addID("AB123456", "passport"));
+    }
+
+    
 }
